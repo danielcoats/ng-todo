61540111e162fcbdf51d9784d8576b0085286c15
Daniel Coats <danielcoats@Daniels-MacBook-Pro.local>
2016-11-21 10:35:54 +1300

    Basic todo app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { TodoService } from './todo.service';
+import {Todo} from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoService]
 })
 export class AppComponent {
-  title = 'app works!';
+
+  newTodo: Todo = new Todo();
+
+  constructor(private todoService: TodoService) {
+  }
+
+  addTodo() {
+    this.todoService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoService.getAllTodos();
+  }
 }
